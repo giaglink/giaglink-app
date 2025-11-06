ade272150fc109a98019647e1eb11a9258e02e46
giaglink <godisablegloballink@gmail.com>
2025-11-06 05:02:43 +0100

    Create next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  // By default, Next.js will handle environment variables prefixed with NEXT_PUBLIC_
+  // on the client side. No extra configuration is needed here for them to work.
+};
+
+export default nextConfig;
